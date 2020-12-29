f3b9c82e0d1238ce01753b79efc4f5f78bc91031
Raj Malhotra <//>
2020-12-29 12:49:28 +0100

    Initial Commit

---
+++ Drawing2/Drawing2App.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing2App.swift
+//  Drawing2
+//
+//  Created by Raj Malhotra on 29.12.20.
+//
+
+import SwiftUI
+
+@main
+struct Drawing2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
